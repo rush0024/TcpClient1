418e71096cecf47acecacd998267e187a8d789cf
Rushika Tamrakar <Rushika Tamrakar@LAPTOP-D490EOAI>
2018-10-08 12:08:51 +0200

    Small change

--- TcpClient1/Program.cs
+++ TcpClient1/Program.cs
@@ -12,6 +12,7 @@ namespace TcpClient1
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            // HOla 
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
